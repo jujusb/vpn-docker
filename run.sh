8ba2e16a98b9aa28ab7149e8cb66375e50ae64eb
jujusb <jsantilariob@gmail.com>
2026-06-30 07:59:44 +0000

    Refactor VPN configuration and remove unused Caddy files

--- run.sh
+++ run.sh
@@ -8,25 +8,7 @@ ensure_network() {
     fi
 }
 ensure_network caddy_net
-ensure_network wg
 
-is_ip() {
-  [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]
-}
-
-if [ "$MULTIPLE_CADDY_GLOBAL" = "true" ]; then
-    echo "Multiple global services enabled → using global Caddyfile"
-    export COMPOSE_FILE="docker-compose.yml"
-else
-    echo "Single global service → using local Caddyfile"
-    export COMPOSE_FILE="docker-compose.yml:docker-compose.root-caddy.yml"
-    if is_ip "$SERVER_ADDRESS"; then
-        echo "Detected IP → using internal TLS"
-        export TLS_CONFIG='tls internal'
-    else
-        echo "Detected domain → using Cloudflare TLS"
-        export TLS_CONFIG='tls { dns cloudflare {env.CF_API_TOKEN} }'
-    fi
-fi
+export COMPOSE_FILE="docker-compose.yml"
 docker compose $@
 unset COMPOSE_FILE
